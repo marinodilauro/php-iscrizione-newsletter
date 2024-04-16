61c004f773eeb11892b0b49f23d19be51e4326a9
marinodilauro <m.dilauro90@gmail.com>
2024-04-16 16:57:46 +0200

    Add content to the page
    - Add placeholder jumbotron
    - Add placeholder posts section
    - Add placeholder footer section

--- index.php
+++ index.php
@@ -36,6 +36,9 @@ var_dump($response);
   <!-- Bootstrap CSS link -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 
+  <!-- FontAwesome 6.2.0 CSS -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
 
   <title>php-iscrizione-newsletter</title>
 </head>
@@ -96,6 +99,73 @@ var_dump($response);
 
     <?php endif; ?>
 
+
+    <!-- Jumbotron -->
+    <div class="p-5 mb-4 bg-light rounded-3">
+      <div class="container-fluid py-5">
+        <h1 class="display-5 fw-bold">Jumbotron</h1>
+        <p class="col-md-8 fs-4">
+          Using a series of utilities, you can create this jumbotron, just
+          like the one in previous versions of Bootstrap. Check out the
+          examples below for how you can remix and restyle it to your liking.
+        </p>
+        <button class="btn btn-primary btn-lg" type="button">
+          Example button
+        </button>
+      </div>
+    </div>
+
+    <!-- Posts -->
+    <section class="posts my-5">
+
+      <div class="container">
+        <div class="row row-cols-1 row-cols-md-2 row-cols-lg-4">
+
+          <div class="col">
+            <div class="card">
+              <img class="card-img-top" src="https://picsum.photos/200" alt="Title" />
+              <div class="card-body">
+                <h4 class="card-title">Title</h4>
+                <p class="card-text">Text</p>
+              </div>
+            </div>
+          </div>
+
+          <div class="col">
+            <div class="card">
+              <img class="card-img-top" src="https://picsum.photos/200" alt="Title" />
+              <div class="card-body">
+                <h4 class="card-title">Title</h4>
+                <p class="card-text">Text</p>
+              </div>
+            </div>
+          </div>
+
+          <div class="col">
+            <div class="card">
+              <img class="card-img-top" src="https://picsum.photos/200" alt="Title" />
+              <div class="card-body">
+                <h4 class="card-title">Title</h4>
+                <p class="card-text">Text</p>
+              </div>
+            </div>
+          </div>
+
+          <div class="col">
+            <div class="card">
+              <img class="card-img-top" src="https://picsum.photos/200" alt="Title" />
+              <div class="card-body">
+                <h4 class="card-title">Title</h4>
+                <p class="card-text">Text</p>
+              </div>
+            </div>
+          </div>
+
+        </div>
+      </div>
+
+    </section>
+
     <!-- Newsletter -->
     <div class="newsletter bg-light">
 
@@ -138,6 +208,101 @@ var_dump($response);
       </div>
     </div>
   </main>
+
+  <footer id="footer" class="bg-dark py-5">
+
+    <div class="container">
+      <div class="row justify-content-between">
+
+        <!-- Address Info -->
+        <div class="col-3">
+          <ul class="list-unstyled">
+
+            <li class="list_title text-light fs-4 fw-bold mb-3">Address Info</li>
+
+            <li class="lh-lg mb-2">
+              <i class="fa-solid fa-location-dot pe-3 text-light"></i>
+              <a class="text-decoration-none text-light" href="">60 29th Street San Francisco. CA 94110 507-Union Trade Center, United States of America</a>
+            </li>
+
+            <li class="lh-lg mb-2">
+              <i class="fa-solid fa-phone pe-3 text-light"></i>
+              <a class="text-decoration-none text-light" href="">(+00) 123-456-789</a>
+            </li>
+
+            <li class="lh-lg mb-2">
+              <i class="fa-solid fa-envelope pe-3 text-light"></i>
+              <a class="text-decoration-none text-light" href="">demo@example.com</a>
+            </li>
+
+          </ul>
+        </div>
+
+        <!-- Informations -->
+        <div class="col-3">
+          <ul class="list-unstyled m-0">
+
+            <li class="list_title text-light fs-4 fw-bold mb-3">Informations</li>
+
+            <li class="lh-lg mb-2">
+              <a class="text-decoration-none text-light" href="">Bookmarks</a>
+            </li>
+
+            <li class="lh-lg mb-2">
+              <a class="text-decoration-none text-light" href="">Features</a>
+            </li>
+
+            <li class="lh-lg mb-2">
+              <a class="text-decoration-none text-light" href="">Sitemap</a>
+            </li>
+
+            <li class="lh-lg mb-2">
+              <a class="text-decoration-none text-light" href="">Lists</a>
+            </li>
+
+            <li class="lh-lg mb-2">
+              <a class="text-decoration-none text-light" href="">Contact Us</a>
+            </li>
+
+          </ul>
+        </div>
+
+        <!-- Extras -->
+        <div class="col-3">
+          <ul class="list-unstyled m-0">
+
+            <li class="list_title text-light fs-4 fw-bold mb-3">Extras</li>
+
+            <li class="lh-lg mb-2">
+              <a class="text-decoration-none text-light" href="">Delivery</a>
+            </li>
+
+            <li class="lh-lg mb-2">
+              <a class="text-decoration-none text-light" href="">Cart</a>
+            </li>
+
+            <li class="lh-lg mb-2">
+              <a class="text-decoration-none text-light" href="">Terms and conditions</a>
+            </li>
+
+            <li class="lh-lg mb-2">
+              <a class="text-decoration-none text-light" href="">My account</a>
+            </li>
+
+            <li class="lh-lg mb-2">
+              <a class="text-decoration-none text-light" href="">About Us</a>
+            </li>
+
+          </ul>
+        </div>
+
+
+
+      </div>
+    </div>
+
+
+  </footer>
 
 </body>
 
