159595effea6301e0f19b00d5f4ad7bb5d557de1
marinodilauro <m.dilauro90@gmail.com>
2024-04-16 19:18:06 +0200

    Remove search bar from header

--- partials/header.php
+++ partials/header.php
@@ -2,25 +2,34 @@
   <header>
 
     <nav class="navbar navbar-expand-sm navbar-dark bg-dark">
+
       <div class="container">
-        <a class="navbar-brand" href="#">Newsletter</a>
+
+        <a class="navbar-brand" href="./index.php">Newsletter</a>
+
         <button class="navbar-toggler d-lg-none" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavId" aria-controls="collapsibleNavId" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
+
         <div class="collapse navbar-collapse" id="collapsibleNavId">
-          <ul class="navbar-nav me-auto mt-2 mt-lg-0">
+
+          <ul class="navbar-nav ms-auto mt-2 mt-lg-0">
+
             <li class="nav-item">
               <a class="nav-link active" href="./index.php" aria-current="page">Home
                 <span class="visually-hidden">(current)</span></a>
             </li>
+
             <li class="nav-item">
               <a class="nav-link active" href="./about.php" aria-current="page">About
                 <span class="visually-hidden">(current)</span></a>
             </li>
+
             <li class="nav-item">
               <a class="nav-link active" href="./contacts.php" aria-current="page">Contacts
                 <span class="visually-hidden">(current)</span></a>
             </li>
+
             <li class="nav-item dropdown">
               <a class="nav-link dropdown-toggle" href="#" id="dropdownId" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown</a>
               <div class="dropdown-menu" aria-labelledby="dropdownId">
@@ -28,13 +37,8 @@
                 <a class="dropdown-item" href="#">Action 2</a>
               </div>
             </li>
+
           </ul>
-          <form class="d-flex my-2 my-lg-0">
-            <input class="form-control me-sm-2" type="text" placeholder="Search" />
-            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">
-              Search
-            </button>
-          </form>
         </div>
       </div>
     </nav>
